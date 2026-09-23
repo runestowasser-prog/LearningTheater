30ba92a8399ed1e31fdc82554c6c1a9f15d6f6f2
runestowasser-prog <rune.stowasser@gmail.com>
2026-09-23 11:02:21 +0200

    added seekbar and play/pause button

--- js/readymadedefinitions.js
+++ js/readymadedefinitions.js
@@ -1,78 +1,5 @@
 const ReadyMades = [];
 
-ReadyMades.push({
-    id: "NextButton",
-    name: "Next button",
-
-    args: [
-      {
-        type: "string",
-        label: "Text",
-        defaultValue: "Next"
-      },
-      {
-        type: "dropdown",
-        label: "Position",
-        options: ["Top", "Center", "Bottom"],
-        defaultValue: "Bottom"
-      }
-    ],
-
-    create: CreateNextButton
-  }
-)
-
-function CreateNextButton(id, readyMadeArgs) {
-
-  BuildCode.setValue(BuildCode.getValue()+
-  `Actor('${id}','div');
-  ActorProps(thisActor, {
-  Parent: "stage",
-  Width: 200,
-  Height: 200,
-  X: 0,
-  Y: 0,
-  Z: 0,
-  Angle: 0,
-  RotateX: 0,
-  RotateY: 0,
-  RotateZ: 0,
-  ShowOrigin: false,
-  Draggable: false,
-  Color: "#4f74c9",
-  Text: "${readyMadeArgs[0]}",
-  FontSize: 16,
-  Opacity: 100,
-  TranslateX: 0,
-  TranslateY: 0,
-  TranslateZ: 0,
-  Scale: 1,
-  SkewX: 0,
-  SkewY: 0,
-  Overflow: "visible",
-  Shape: "RightArrow",
-  Controls: false,\n});`
-  )
-   GeneratedTriggers.push({
-    event: "MouseUp",
-    target: id,
-    key: "",
-    conditions: [],
-    actions: [
-      {
-        fn: "MoveActor",
-        args: [''+id, 'X', '500', 'power1.out', '1', '0'],
-        code: `Move.to(${id},{X:500,ease:"power1.out",duration:1,delay:0})`
-      }
-    ],
-    comment: "Ready Made Next Button moving"
-  });
-
-  showPreview();
-  ReadyMadeEditor.Opacity=0;
-  
-
-}
 
 
 ReadyMades.push({
@@ -276,3 +203,395 @@ if(readyMadeArgs[0]=="Right"){
   
 
 }
+
+
+
+ReadyMades.push({
+    id: "SeekBar",
+    name: "Seekbar",
+
+    args: [
+     
+  
+      {
+        type: "color",
+        label: "Progress Color",
+      },
+       {
+        type: "color",
+        label: "Background Color",
+      },
+    ],
+
+    create: CreateSeekBar
+  }
+)
+
+function CreateSeekBar(id, readyMadeArgs) {
+
+  var Yposition=StageHeightInput.value-60;
+  var WidthAdjusted=StageWidthInput.value-500;
+
+  BuildCode.setValue(BuildCode.getValue()+
+  `
+  Actor('${id}','div');
+  ActorProps(thisActor, {
+  Parent: "stage",
+  Width: ${WidthAdjusted},
+  Height: 30,
+  X: 200,
+  Y: ${Yposition},
+  CustomProps: [
+    { name: "seeking", value: "false", type:"boolean" },
+  ],
+  seeking: false,
+  });
+
+  Actor('${id}Seekbar','div');
+  ActorProps(thisActor, {
+  Parent: "${id}",
+  Width: ${WidthAdjusted},
+  Height: 30,
+  Style: "border-radius:10em",
+  Color: "${readyMadeArgs[1]}",
+  Shape: "Square",
+  CustomProps: [
+    { name: "progress", value: "0", type:"number" },
+  ],
+  progress: 0,
+  });
+
+  Actor('${id}SeekbarIndicator','div');
+  ActorProps(thisActor, {
+  Parent: "${id}Seekbar",
+  Width: 0,
+  Height: 30,
+  Style: "border-radius:10em",
+  Color: "${readyMadeArgs[0]}",
+  });
+
+  Actor('${id}SeekbarInteraction','div');
+  ActorProps(thisActor, {
+  Parent: "${id}",
+  Width: ${WidthAdjusted},
+  Height: 30,
+  Style: "cursor:pointer",
+  Shape: "Square",
+  CustomProps: [
+    { name: "seeking", value: "false", type:"boolean" },
+  ],
+  seeking: false,
+  });`
+  );
+
+   GeneratedTriggers.push(
+    {
+    "event": "Update",
+    "comment": "Update the Seekbar",
+    "conditions": [],
+    "actions": [
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"SeekbarIndicator",
+          "Width",
+          "=",
+          "(GetScene().Timeline.time() / GetScene().Timeline.duration()) * "+id+"Seekbar.Width"
+        ],
+        "code": +id+"SeekbarIndicator.Width = (GetScene().Timeline.time() / GetScene().Timeline.duration()) * "+id+"Seekbar.Width"
+      }
+    ],
+    "fireMode": "whiletrue"
+  },
+  {
+    "event": "MouseDown",
+    "target": id+"SeekbarInteraction",
+    "comment": "Push the Seekbar",
+    "conditions": [],
+    "actions": [
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"SeekbarInteraction",
+          "seeking",
+          "=",
+          "true"
+        ],
+        "code": id+"SeekbarInteraction.seeking = true"
+      }
+    ],
+    "key": ""
+  },
+  {
+    "event": "MouseUp",
+    "target": id+"SeekbarInteraction",
+    "comment": "Release the Seekbar",
+    "conditions": [],
+    "actions": [
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"SeekbarInteraction",
+          "seeking",
+          "=",
+          "false"
+        ],
+        "code": id+"SeekbarInteraction.seeking = false"
+      }
+    ],
+    "key": ""
+  },
+  {
+    "event": "Update",
+    "comment": "Update the seekbar on interaction",
+    "conditions": [
+      {
+        "fn": "GetProperty",
+        "args": [
+          id+"SeekbarInteraction",
+          "seeking",
+          "==",
+          "true"
+        ],
+        "code": id+"SeekbarInteraction.seeking==true"
+      }
+    ],
+    "actions": [
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"Seekbar",
+          "progress",
+          "=",
+          "(MouseX - "+id+".X) / "+id+"Seekbar.Width"
+        ],
+        "code": id+"Seekbar.progress = (MouseX - "+id+".X) / "+id+"Seekbar.Width"
+      },
+      {
+        "fn": "",
+        "args": [],
+        "code": "GetScene().Timeline.time("+id+"Seekbar.progress * GetScene().Timeline.duration())"
+      }
+    ],
+    "fireMode": "whiletrue"
+    }
+  );
+
+  showPreview();
+  ReadyMadeEditor.Opacity=0;
+  
+
+}
+
+
+ReadyMades.push({
+    id: "playPause",
+    name: "Play/Pause Button",
+
+    args: [
+     
+  
+      {
+        type: "color",
+        label: "Color",
+      },
+    ],
+
+    create: CreatePlayPause
+  }
+)
+
+function CreatePlayPause(id, readyMadeArgs) {
+
+  var Yposition=StageHeightInput.value-90;
+
+  BuildCode.setValue(BuildCode.getValue()+
+  `
+  Actor('${id}','div');
+  ActorProps(thisActor, {
+  Parent: "stage",
+  Width: 90,
+  Height: 100,
+  X: 20,
+  Y: ${Yposition},
+});
+
+Actor('${id}PlayBtn','div');
+ActorProps(thisActor, {
+  Parent: "${id}",
+  Width: 50,
+  Height: 50,
+  X: 20,
+  Y: 20,
+  Color: "${readyMadeArgs[0]}",
+  Shape: "Play",
+});
+
+Actor('${id}PauseBtn','div');
+ActorProps(thisActor, {
+  Parent: "${id}",
+  Width: 50,
+  Height: 50,
+  X: 20,
+  Y: 20,
+  Color: "${readyMadeArgs[0]}",
+  Shape: "Pause",
+});
+
+Actor('${id}PlayPauseSwitch','div');
+ActorProps(thisActor, {
+  Parent: "${id}",
+  Width: 90,
+  Height: 100,
+  Style: "cursor:pointer",
+});
+`
+  );
+  
+   GeneratedTriggers.push(
+    {
+    "event": "MouseUp",
+    "target": id+"PlayPauseSwitch",
+    "comment": "Switch Play/Pause Button",
+    "conditions": [],
+    "actions": [
+      {
+        "fn": "ToggleProperty",
+        "args": [
+          id+"PlayBtn",
+          "Opacity",
+          "100",
+          "0"
+        ],
+        "code": "if ("+id+"PlayBtn.Opacity == 100) {   "+id+"PlayBtn.Opacity = 0;  } else {    "+id+"PlayBtn.Opacity = 100;      }    "
+      },
+      {
+        "fn": "ToggleProperty",
+        "args": [
+          id+"PauseBtn",
+          "Opacity",
+          "0",
+          "100"
+        ],
+        "code": "if ("+id+"PauseBtn.Opacity == 0) {   "+id+"PauseBtn.Opacity = 100;  } else {    "+id+"PauseBtn.Opacity = 0;      }    "
+      }
+    ],
+    "key": "Space"
+  },
+  {
+    "event": "Update",
+    "comment": "show Play when timeline ends",
+    "conditions": [
+      {
+        "fn": "",
+        "args": [],
+        "code": "GetScene().Timeline.time() >= GetScene().Timeline.duration()"
+      }
+    ],
+    "actions": [
+      {
+        "fn": "ToggleProperty",
+        "args": [
+          id+"PlayBtn",
+          "Opacity",
+          "100",
+          "0"
+        ],
+        "code": "if ("+id+"PlayBtn.Opacity == 100) {   "+id+"PlayBtn.Opacity = 0;  } else {    "+id+"PlayBtn.Opacity = 100;      }    "
+      },
+      {
+        "fn": "ToggleProperty",
+        "args": [
+          id+"PauseBtn",
+          "Opacity",
+          "0",
+          "100"
+        ],
+        "code": "if ("+id+"PauseBtn.Opacity == 0) {   "+id+"PauseBtn.Opacity = 100;  } else {    "+id+"PauseBtn.Opacity = 0;      }    "
+      }
+    ],
+    "fireMode": "onceWhileTrue"
+  },
+  {
+    "event": "Update",
+    "comment": "",
+    "conditions": [
+      {
+        "fn": "GetProperty",
+        "args": [
+          id+"PlayBtn",
+          "Opacity",
+          "==",
+          "100"
+        ],
+        "code": id+"PlayBtn.Opacity==100"
+      }
+    ],
+    "actions": [
+      {
+        "fn": "",
+        "args": [],
+        "code": "GetScene().Timeline.pause()"
+      }
+    ],
+    "fireMode": "whiletrue"
+  },
+  {
+    "event": "Update",
+    "comment": "",
+    "conditions": [
+      {
+        "fn": "GetProperty",
+        "args": [
+          id+"PlayBtn",
+          "Opacity",
+          "==",
+          "0"
+        ],
+        "code": id+"PlayBtn.Opacity==0"
+      }
+    ],
+    "actions": [
+      {
+        "fn": "",
+        "args": [],
+        "code": "GetScene().Timeline.play()"
+      }
+    ],
+    "fireMode": "whiletrue"
+  },
+  {
+    "event": "SceneStart",
+    "comment": "Play on Scene Start",
+    "conditions": [],
+    "actions": [
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"PlayBtn",
+          "Opacity",
+          "=",
+          "0"
+        ],
+        "code": id+"PlayBtn.Opacity = 0"
+      },
+      {
+        "fn": "SetProperty",
+        "args": [
+          id+"PauseBtn",
+          "Opacity",
+          "=",
+          "100"
+        ],
+        "code": id+"PauseBtn.Opacity = 100"
+      }
+    ]
+  }
+  );
+
+  showPreview();
+  ReadyMadeEditor.Opacity=0;
+  
+
+}
+
